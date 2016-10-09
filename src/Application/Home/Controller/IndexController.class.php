94c67a0d03b8b2f7c97cd7851970a4ef90e50fdd
Sam Liu <shan3275@gmail.com>
2016-10-09 15:07:06 +0800

    修改默认广告投放的默认配置

--- src/Application/Home/Controller/IndexController.class.php
+++ src/Application/Home/Controller/IndexController.class.php
@@ -109,13 +109,13 @@ class IndexController extends CommonController {
         {
             //refer 为空,作为调试使用
             \Think\Log::write(sprintf("%s(%d) refer为空,不查询需求规则,走默认规则!",__FUNCTION__ , __LINE__), 'DEBUG');
-            $url = 'http://219.234.83.60/dsp/default.html';
+            $url = 'http://219.234.83.60/sc/default.html';
         }
 
         //需求没有命中,设置默认投放链接
         if ($url == null)
         {
-            $url = 'http://219.234.83.60/dsp/default.html';
+            $url = 'http://219.234.83.60/sc/default.html';
             \Think\Log::write(sprintf("%s(%d) 全部需求都没有命中,走默认投放链接!",__FUNCTION__ , __LINE__), 'DEBUG');
         }
 
